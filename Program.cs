a4429e834efc811fb0a708d00d9ec127d80b0470
Popov Danil <PopovDanil123@gmail.com>
2025-05-13 16:54:11 +0600

    Первый коммит

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿public class Test
+{
+    public int Prop1 { get; set; }
+
+    public int Prop2 { get; set; }
+
+    public Test() { }
+
+    public void ChangeFieldsValue(int Prop1,int Prop2)
+    {
+        this.Prop1 = Prop1;
+        this.Prop2 = Prop2;
+        Console.WriteLine("Значения полей успешно изменены");
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Test test = new Test();
+        int newProp = int.Parse(Console.ReadLine());
+        int secondNewProp = int.Parse(Console.ReadLine());
+        test.ChangeFieldsValue(newProp, secondNewProp);
+        Console.WriteLine($"Prop1: {test.Prop1}, Prop2: {test.Prop2}");
+        Console.ReadKey();
+    }
+}
